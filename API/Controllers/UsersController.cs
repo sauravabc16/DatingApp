cba28d3ec8807ed2b251d8f991f6ab53748b64d5
Saurav Kumar <sauravabc16@yahoo.co.in>
2022-07-30 18:59:18 +0530

    made db calls async for scalability pourposes (#1)

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -19,14 +20,14 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers(){
-            return _context.Users.ToList();
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+            return await _context.Users.ToListAsync();
         }
 
         // api/users/3
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id){
-            return _context.Users.Find(id);
+        public async Task <ActionResult<AppUser>> GetUser(int id){
+            return await _context.Users.FindAsync(id);
         }
 
     }
