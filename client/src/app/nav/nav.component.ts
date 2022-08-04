dbca50dc89d55c8b11c4ac3cfdba5a2d55303129
Saurav Kumar <sauravabc16@yahoo.co.in>
2022-08-04 21:46:14 +0530

    using async pipe to prevent memory leak (#2)

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,17 +10,14 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model:any={}
-  constructor(private accountService: AccountService) { }
-  loggedIn: boolean;
+  constructor(public accountService: AccountService) { }
+
 
   ngOnInit(): void {
-    this.getCurrentUser();
-
   }
   login(){
     this.accountService.login(this.model).subscribe(response =>{
       console.log(response);
-      this.loggedIn = true;
     },error=>{
       console.log(error);
     })
@@ -26,14 +25,5 @@ export class NavComponent implements OnInit {
 
   logout(){
     this.accountService.logout();
-    this.loggedIn=false;
-  }
-
-  getCurrentUser(){
-    this.accountService.currentUser$.subscribe(user =>{
-      this.loggedIn = !!user;
-    },error=>{
-      console.log(error)
-    })
   }
 }
