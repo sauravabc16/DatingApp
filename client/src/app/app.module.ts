868826aeadcf157a4265583808af438e22a26430
Saurav Kumar <sauravabc16@yahoo.co.in>
2022-08-14 23:08:07 +0530

    *temp commit*

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,7 +14,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import {ToastrModule} from 'ngx-toastr';
+//import {ToastrModule} from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -33,10 +33,7 @@ import {ToastrModule} from 'ngx-toastr';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot(),
-    ToastrModule.forRoot({
-      positionClass: 'toast-bottom-right'
-    })
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
